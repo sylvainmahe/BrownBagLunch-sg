84d04021e29e4ba6bb1e69cbdbba35aeb0ce26f9
Eric Lefevre-Ardant <contact@ericlefevre.net>
2015-02-04 08:28:02 +0800

    Take images for cities optionally

--- js/baggers.js
+++ js/baggers.js
@@ -20,7 +20,7 @@ var data = {
                     "summary": "Une promenade décousue dans les limites du code source Java, à la manière de Douglas Hofstadter. Nous parlons de code, d'apparences trompeuses, d'art et de la vie."
                 },
                 {
-                    "title": "Extreme Programming - avec Cyrille Martraire",
+                    "title": "Extremist Programming",
                     "summary": "Bien sûr, notre métier est sérieux ; le Software Craftmanship n'est-il pas avant tout le professionnalisme ? Bien sûr, le progrès vient d'articles scientifiques ardus et de nouveaux logiciels complexes. Mais vous ne rêvez pas parfois de repousser le cadre professionnel habituel ? D'aller plus loin et d'explorer ? De vous amuser avec du code jusqu'à ce que ça devienne n'importe quoi ? Dans cette session nous vous présenterons des expériences ésotériques, des jeux bizarres et des joyeux délires qui font réfléchir. Réfléchir ? D'habitude on n'a jamais le temps !"
                 }
             ],
@@ -36,7 +36,6 @@ var data = {
     "cities": [
         {
             "name": "Geylang",
-            "ville_img": "img/villes/BBL.jpg",
             "lat": 1.319718,
             "lng": 103.891354
         }
